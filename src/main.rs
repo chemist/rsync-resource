454d414439eb8002dcfc1351e06e5605bd87ccc3
chemist <chemistmail@gmail.com>
2017-03-16 15:24:21 +0000

    fix

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@ extern crate rustc_serialize;
 //use rustc_serialize::json;
 use std::io;
 use std::io::prelude::*;
-//use std::collections::BTreeMap;
 use std::fmt;
 use rustc_serialize::json::{self};
+use std::process::Command;
+use std::process::Output;
 
 //Automatically generate `RustcDecodable` and `RustcEncodable` trait
 // implementations
@@ -60,5 +61,27 @@ fn concourse_check() {
         .expect("Failed to read input");
     let check: Check = json::decode(&input).unwrap();
     log!("Input is: {:?}", check);
+    let input: String = format!("rsync://{}/{}",check.source.server, check.source.base_dir);
+    let rsync = Command::new("rsync")
+        .arg(input)
+        .output()
+        .expect("cant check rsync server");
+    let result = get_versions(&rsync);
+    log!("rsync: {:?}", result);
     println!("[]")
 }
+
+fn get_versions(rsync: &Output) -> Vec<Version> {
+    let folders = String::from_utf8_lossy(&rsync.stdout);
+    let mut result = Vec :: new();
+    for one in folders.lines() {
+        let ver = Version {
+            data: one.split_whitespace().last().expect("cant split str").to_string(),
+        };
+        if ver.data != "." {
+            result.push(ver);
+        }
+    }
+    return result;
+}
+
